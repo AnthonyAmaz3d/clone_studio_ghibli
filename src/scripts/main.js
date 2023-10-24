9bbafee74231d438e0971e9720bf92c90e835c9e
Anthony <anthonymaia00@gmail.com>
2023-10-23 21:52:13 -0300

    criando a faq

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelectorAll('[data-tab-button]');
+    const questions = document.querySelectorAll('[data-faq-question]');
+
+        // seção de shows ou atraçÕes
 
     for (let i = 0; i < button.length; i++ ) {
         button[i].addEventListener('click', function(botao) {
@@ -11,7 +14,24 @@ document.addEventListener('DOMContentLoaded', function () {
             botao.target.classList.add('shows__tabs__button--is-active');
         })
     }
+
+    // seção de FAQ (accordion)
+
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', closeAndOpen);
+    }
 })
+
+// função para abrir e fechar o FAQ
+
+const closeAndOpen = (elemento) => {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
+
+//função para remover a classe --is-active do botão 
 
 const removeBotaoAtivo = () => {
     const buttons = document.querySelectorAll('[data-tab-button]');
@@ -20,6 +40,8 @@ const removeBotaoAtivo = () => {
         buttons[i].classList.remove('shows__tabs__button--is-active');
     }
 }
+
+// função para remover a classe --is-active dos tabs
 
 const escondeTodasAbas = () => {
     const tabContainer = document.querySelectorAll('[data-tab-id]');
