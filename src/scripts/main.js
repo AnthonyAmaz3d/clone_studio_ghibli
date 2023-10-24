3ea8eb45d860b94a9a7158334e324f04f2b8baae
Anthony <anthonymaia00@gmail.com>
2023-10-24 14:36:58 -0300

    criando o header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const button = document.querySelectorAll('[data-tab-button]');
     const questions = document.querySelectorAll('[data-faq-question]');
 
+    // animação header
+
+    const heroSection = document.querySelector('.hero');
+    const alturaHeroSection = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const alturaScrollY = window.scrollY;
+
+        if (alturaScrollY < alturaHeroSection) {
+            ocultaElemento();
+        } else {
+            exibeElemento();
+        }
+    })
+
+
         // seção de shows ou atraçÕes
 
     for (let i = 0; i < button.length; i++ ) {
@@ -21,6 +37,20 @@ document.addEventListener('DOMContentLoaded', function () {
         questions[i].addEventListener('click', closeAndOpen);
     }
 })
+
+// função para ocultar elementos do header
+
+const ocultaElemento = () => {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+// função para exiber elementos do header
+
+const exibeElemento = () => {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
 
 // função para abrir e fechar o FAQ
 
